45888685d887a7d6db78e753053599b9a822755d
luizfernando <lf.nandooliveeira@gmail.com>
2026-03-26 16:01:01 -0300

    probabilistic retrieval model aplicado sem abstração usando bm25

---
+++ fundamentos/tokenization-probabilistc-retrieval.py
@@ -0,0 +1,55 @@
+"""
+A utilização do modelo de retrieval probabilístico é realizar a recuperação dos documentos mais relevantes com base em probabilidades de ocorrência de termos.
+
+Por exemplo, o corpo desse repositório é composto por documentos com frases de variadas sobre o universo de TI.
+
+Quando o sistema recebe uma query buscando algo no tipo: "machine learning", o sistema de retrieval é capaz de
+buscar e retornar os documentos mais relevantes a essa query, mesmo que não contenham exatamente os termos "machine" e "learning", mas sim termos relacionados.
+
+"""
+
+import nltk
+import numpy as np
+from rank_bm25 import BM25Okapi
+
+documents = [
+    "Machine learning é um campo da inteligência artificial que permite que computadores aprendam padrões a partir de dados.",
+    "O aprendizado de máquina dá aos sistemas a capacidade de melhorar seu desempenho sem serem explicitamente programados.",
+    "Em vez de seguir apenas regras fixas, o machine learning descobre relações escondidas nos dados.",
+    "Esse campo combina estatística, algoritmos e poder computacional para extrair conhecimento.",
+    "O objetivo é criar modelos capazes de generalizar além dos exemplos vistos no treinamento.",
+    "Aplicações de machine learning vão desde recomendações de filmes até diagnósticos médicos.",
+    "Os algoritmos de aprendizado de máquina transformam dados brutos em previsões úteis.",
+    "Diferente de um software tradicional, o ML adapta-se conforme novos dados chegam.",
+    "O aprendizado pode ser supervisionado, não supervisionado ou por reforço, dependendo do tipo de problema.",
+    "Na prática, machine learning é o motor que impulsiona muitos avanços em visão computacional e processamento de linguagem natural.",
+    "Mais do que encontrar padrões, o machine learning ajuda a tomar decisões baseadas em evidências.",
+]
+
+
+def preprocess(text):
+    text_lower = text.lower()
+
+    tokens = nltk.word_tokenize(text_lower)
+
+    return [word for word in tokens if word.isalnum()]
+
+
+tokenized_docs = [preprocess(doc) for doc in documents]
+
+bm25 = BM25Okapi(tokenized_docs)
+
+query = "machine learning"
+
+
+def search_bm25(query, bm25):
+    tokenized_query = preprocess(query)
+    results = bm25.get_scores(tokenized_query)
+    return results
+
+
+results = search_bm25(query, bm25)
+results
+
+for i in np.argsort(results)[::-1]:
+    print(f"Documento: {i}: {documents[i]}")
